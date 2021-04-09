8d177cdfda1efacb6e1beaa9e3f3c25c3d5c42d5
Evan S. Gonzalez <evangonzalez.mail@gmail.com>
2021-04-09 17:45:22 -0400

    Add cassert header.

--- src/parareal/parareal.cpp
+++ src/parareal/parareal.cpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <cassert>
 
 #include "parareal/parareal.hpp"
 
@@ -8,16 +9,16 @@ Parareal::TimeBins Parareal::generateFineTime(Parareal::TimeIndex n) {
   TimeBins fine_time;
 
   assert(n < _coarse_time.size() - 1);
-  
+
+  // push back the time steps before index n
   for (int i = 0; i < n; i++) {
     fine_time.push_back(_coarse_time.at(i));
   }
 
-  TimeBins new_time = linspace(_coarse_time.at(n),
-			       _coarse_time.at(n+1),
-			       _n_fine_per_coarse+1);
-
-  for (const auto& t : new_time) {
+  // push back the new time steps
+  for (const auto& t : linspace(_coarse_time.at(n),
+				_coarse_time.at(n+1),
+				_n_fine_per_coarse+1) {
     fine_time.push_back(t);
   }
     
